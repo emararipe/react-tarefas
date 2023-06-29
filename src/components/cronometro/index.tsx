53f5656fb7e152427e3c0ea5e8bc931ac136ec48
emararipe <emararipe@gmail.com>
2023-06-28 23:04:09 -0300

    função contagem regressiva

--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -9,23 +9,33 @@ interface Props {
   selecionado: Itarefa | undefined
 }
 
-function Cronometro({selecionado}: Props) {
+function Cronometro({ selecionado }: Props) {
   const [tempo, setTempo] = useState<number>()
   useEffect(() => {
-    if(selecionado?.tempo){
+    if (selecionado?.tempo) {
       setTempo(tempoParaSeegundos(selecionado.tempo))
     }
   }, [selecionado])
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1)
+        return regressiva(contador - 1)
+      }
+    }, 1000)
+  }
 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
-        <Relogio tempo={tempo}/>
+        <Relogio tempo={tempo} />
       </div>
-      <Botao>Começar!</Botao>
+      <Botao onClick={() => regressiva(tempo)}>Começar!</Botao>
     </div>
   )
 }
 
 export default Cronometro
+
